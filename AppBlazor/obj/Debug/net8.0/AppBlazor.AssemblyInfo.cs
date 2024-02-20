1db1f46f1f5f57e19307c917c725e7abf634d6c5
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-02-20 00:43:59 -0400

    it works!!!!!!!!!!

--- AppBlazor/obj/Debug/net8.0/AppBlazor.AssemblyInfo.cs
+++ AppBlazor/obj/Debug/net8.0/AppBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b752f6757fc9c59e868bbe8e74472a67fa795d8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a17738a2759375f0073409c67fbf2c2939839662")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
