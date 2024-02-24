4d6eecf85432fe60dd7eef0985e9aff9373e5c6f
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-02-24 14:50:38 -0400

    con esto

---
+++ AppBlazor/Components/Pages/Personaje/PersonajesBase.cs
@@ -0,0 +1,16 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using AppBlazor.Data.Models;
+
+namespace AppBlazor.Components
+{
+    public class PersonajesBase : ComponentBase
+    {
+        public List<Personaje> listaPersonajes {get; set;}
+
+        protected override async Task OnInitializedAsync() {
+            
+        }
+    }
+}
